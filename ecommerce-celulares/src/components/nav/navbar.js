390d9dbe937118ba786f95f7f7b8dab47d7349ac
PabloRuizS2 <pabloruizs2002@gmail.com>
2022-08-22 18:50:52 -0300

    primera entrega2

---
+++ ecommerce-celulares/src/components/nav/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+                <ul>
+                    <li><a href="">Productos</a></li>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Contacto</a></li>           
+                </ul>
+            </nav>   
+  )
+}
+
+export default navbar
